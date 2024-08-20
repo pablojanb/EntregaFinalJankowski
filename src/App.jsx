f5b51d13253f6e19a887041fbb3cbf49bf95f330
pabloj <pablojanb@gmail.com>
2024-08-20 17:40:27 -0300

    agrego footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Footer } from "./components/Footer";
 import { ItemDetailContainer } from "./components/ItemDetailContainer";
 import { ItemListContainer } from "./components/ItemListContainer";
 import { NavBar } from "./components/NavBar";
@@ -18,6 +19,7 @@ function App() {
         <Route path="/category/:id" element={<ItemListContainer />}/>
         <Route path="/item/:id" element={<ItemDetailContainer />}/>
       </Routes>
+      <Footer />
     </BrowserRouter>
     </>
   )
---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export const Footer = ()=>{
+    return (
+        <div className="footer">
+            <p>Todos los derechos reservados</p>
+        </div>
+            )
+}
